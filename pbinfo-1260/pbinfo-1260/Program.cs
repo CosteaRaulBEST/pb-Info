2d5eb429bc18937bc751447594125796d67094e5
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-24 20:14:02 +0300

    Add files via upload

---
+++ pbinfo-1260/pbinfo-1260/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_1260
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a+b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+
+
+        }
+    }
+}
