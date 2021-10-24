cd6557c2857c619e2111d9abec0800fb477dbdfc
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-24 23:07:08 +0300

    Add files via upload

---
+++ pbinfo-2263/pbinfo-2263/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pbinfo_2263
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int t1 = int.Parse(Console.ReadLine());
+            int t2 = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine(t1*m*z+t2*n*z);
+
+        }
+    }
+}
