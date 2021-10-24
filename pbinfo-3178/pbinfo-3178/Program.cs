a26e3a75505db2438cd4383180af857159110830
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-24 22:09:11 +0300

    Add files via upload

---
+++ pbinfo-3178/pbinfo-3178/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_3178
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((f * 3 * n) + (b * 2 * n));
+
+        }
+    }
+}
