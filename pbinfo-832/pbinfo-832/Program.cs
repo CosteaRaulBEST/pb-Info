d26572d8b720a52d6325ccb5e4fb5915a8e4253a
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-24 23:04:22 +0300

    Add files via upload

---
+++ pbinfo-832/pbinfo-832/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pbinfo_832
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if ( n >= 5)
+            {
+                Console.WriteLine("promovat");
+            }
+            else Console.WriteLine("respins");
+        }
+    }
+}
