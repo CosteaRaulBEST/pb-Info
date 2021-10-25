0d30ec879d5032863e16313a60da1431bda826cb
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 14:53:16 +0300

    Add files via upload

---
+++ pbinfo-2377/pbinfo-2377/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pbinfo_2377
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine( n );
+
+        }
+    }
+}
