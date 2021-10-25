611337f71a2c569d232c55b139f98739c1d60b00
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 12:26:27 +0300

    Add files via upload

---
+++ pbinfo-3063/pbinfo-3063/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_3063
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1)
+                Console.WriteLine("ianuarie");
+            if (n == 2)
+                Console.WriteLine("februarie");
+            if (n == 3)
+                Console.WriteLine("martie");
+            if (n == 4)
+                Console.WriteLine("aprilie");
+            if (n == 5)
+                Console.WriteLine("mai");
+            if (n == 6)
+                Console.WriteLine("iunie");
+            if (n == 7)
+                Console.WriteLine("iulie");
+            if (n == 8)
+                Console.WriteLine("august");
+            if (n == 9)
+                Console.WriteLine("septembrie");
+            if (n == 10)
+                Console.WriteLine("octombrie");
+            if (n == 11)
+                Console.WriteLine("noiembrie");
+            if (n == 12)
+                Console.WriteLine("decembire");
+        }
+    }
+}
