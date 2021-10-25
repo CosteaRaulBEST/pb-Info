0947e1a6cb7849d6f9ba06ea313660a648572fb8
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 12:24:32 +0300

    Add files via upload

---
+++ pbinfo-168/pbinfo-168/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_168
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if ((n <= 0) && (b <= 0))
+            {
+                Console.WriteLine("DA");
+            }
+            else if ((n >= 0) && (b >= 0))
+            {
+                Console.WriteLine("DA");
+            }
+            else
+            {
+                Console.WriteLine("NU");
+            }
+        }
+    }
+}
