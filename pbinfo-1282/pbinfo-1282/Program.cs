f4d3a05658d89b513d876fc2c078becf04b78226
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 14:55:48 +0300

    Add files via upload

---
+++ pbinfo-1282/pbinfo-1282/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_1282
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(sqrt(n));
+        }
+
+        private static bool sqrt(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
