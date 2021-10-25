744c6fd651ed97a8269e8c283fd286a74d5ca8b2
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 14:54:59 +0300

    Add files via upload

---
+++ pbinfo-102/pbinfo-102/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pbinfo_102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int cz, cu;
+            cu = n % 10;
+            n = n / 10;
+            cz = n % 10;
+            Console.WriteLine(cz+cu);
+
+        }
+    }
+}
