08e75f73608276122010d8763b6728e2792a4b9e
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 12:25:06 +0300

    Add files via upload

---
+++ pbinfo-452/pbinfo-452/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_452
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int s = 0;
+            if (n % 10 % 2 == 1)
+            {
+                s = s + 1;
+            }
+            n = n / 10;
+            if (n % 10 % 2 == 1)
+            {
+                s = s + 1;
+            }
+            n = n / 10;
+            if (n % 10 % 2 == 1)
+            {
+                s = s + 1;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
