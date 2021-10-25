8748494b3cb609bee3ab02d12b4030606adc9ba8
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-25 14:56:14 +0300

    Add files via upload

---
+++ pbinfo-3210/pbinfo-3210/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_3210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine( m / n );
+            Console.WriteLine( n - (m % n ) ) ;
+
+        }
+    }
+}
