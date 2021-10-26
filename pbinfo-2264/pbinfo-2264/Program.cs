cc3fc3e8c4bcb6d84ff60fb98a68ab1733d62b03
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-26 13:39:25 +0300

    Add files via upload

---
+++ pbinfo-2264/pbinfo-2264/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_2264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int uc1, uc2;
+            uc1 = n % 10;
+            n = n / 10;
+            uc2 = n % 10;
+            Console.WriteLine( uc1 + uc2 );
+
+        }
+    }
+}
