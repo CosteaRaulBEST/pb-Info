276e2d8b7c0821778f72bd6da294dbe712660208
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-26 13:38:50 +0300

    Add files via upload

---
+++ pbinfo-169/pbinfo-169/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_169
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int uc1, uc2, uc3;
+            uc1 = n % 10;
+            n = n / 10;
+            uc2 = n % 10;
+            n = n / 10;
+            uc3 = n % 10;
+            Console.WriteLine(uc1+uc2+uc3);
+        }
+    }
+}
