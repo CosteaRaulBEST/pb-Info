4a387d6788df4848d0ff243f02972c30d08996ea
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-26 13:40:36 +0300

    Add files via upload

---
+++ pbinfo-468/pbinfo-468/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_468
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int uc1 , uc2 , p ;
+            uc1 = n % 10;
+            n = n / 100;
+            uc2 = n % 10;
+            p = 10 * uc2 + uc1;
+            Console.WriteLine( p * p );
+        }
+    }
+}
