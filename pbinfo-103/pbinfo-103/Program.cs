f30be1b750c10fc9494f124fe2cadac2e6c74c97
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-26 13:40:14 +0300

    Add files via upload

---
+++ pbinfo-103/pbinfo-103/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine( n * m );
+            Console.WriteLine(2 * n + 2 * m);
+        }
+    }
+}
