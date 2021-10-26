cfd89bb67f7e0f0f21df459aae9d0bb88018bcc5
Costea Raul <91869309+CosteaRaulBEST@users.noreply.github.com>
2021-10-26 13:39:07 +0300

    Add files via upload

---
+++ pbinfo-2601/pbinfo-2601/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbinfo_2601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int uc1, uc2, uc3;
+            uc1 = n % 10;
+            n = n / 10;
+            uc2 = n % 10;
+            n = n / 10;
+            uc3 = n % 10;
+            Console.WriteLine(uc1 * uc1 + uc2 * uc2 + uc3 * uc3);
+
+        }
+    }
+}
